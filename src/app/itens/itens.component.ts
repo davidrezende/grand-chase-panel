6eca15ca1da04f80668db5ca5b8fa767ee6c3b18
davidrezende <davidpenalives@gmail.com>
2020-08-22 04:35:34 -0300

    Completed functionality to add temporary, stacked and fortified items

--- src/app/itens/itens.component.ts
+++ src/app/itens/itens.component.ts
@@ -12,48 +12,92 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./itens.component.css']
 })
 export class ItensComponent implements OnInit {
+  rbStack;
+  rbTemporary;
+
+  // rbStack = "normal";
+  // rbTemporary = "perma";
+
+  itemFromPanel = {} as ItemPanel;
   user = {} as User;
   item = {} as Item;
 
-  textAreaItems: string;
   arrayItems = {} as string[];
+  textAreaItems: string;
   raritySelect: number;
   timeItemInput: number;
   amountStackInput: number;
 
-  itemFromPanel = {} as ItemPanel;
+  optionsRarity = [];
+  optionsStrength = [];
 
-  optionsRarity = [
-    { name: "Normal", value: 0 },
-    { name: "Raro", value: 1 },
-    { name: "Épico", value: 2 },
-    { name: "Lendário", value: 3 }
-  ]
+  // optionsRarity = [
+  //   { name: "Raridade", value: -1 },
+  //   { name: "Normal", value: 0 },
+  //   { name: "Raro", value: 1 },
+  //   { name: "Épico", value: 2 },
+  //   { name: "Lendário", value: 3 }
+  // ]
 
-  optionsStrength = [
-    { value: 1 },
-    { value: 2 },
-    { value: 3 },
-    { value: 4 },
-    { value: 5 },
-    { value: 6 },
-    { value: 7 },
-    { value: 8 },
-    { value: 9 },
-    { value: 10 },
-    { value: 11 },
-    { value: 12 },
-    { value: 13 },
-    { value: 14 },
-    { value: 15 },
-    { value: 16 },
-    { value: 17 }
-  ]
-
+  // optionsStrength = [
+  //   { name: "Fortificação", value: -1 },
+  //   { name: "Nível 1", value: 1 },
+  //   { name: "Nível 2", value: 2 },
+  //   { name: "Nível 3", value: 3 },
+  //   { name: "Nível 4", value: 4 },
+  //   { name: "Nível 5", value: 5 },
+  //   { name: "Nível 6", value: 6 },
+  //   { name: "Nível 7", value: 7 },
+  //   { name: "Nível 8", value: 8 },
+  //   { name: "Nível 9", value: 9 },
+  //   { name: "Nível 10", value: 10 },
+  //   { name: "Nível 11", value: 11 },
+  //   { name: "Nível 12", value: 12 },
+  //   { name: "Nível 13", value: 13 },
+  //   { name: "Nível 14", value: 14 },
+  //   { name: "Nível 15", value: 15 },
+  //   { name: "Nível 16", value: 16 },
+  //   { name: "Nível 17", value: 17 }
+  // ]
 
   constructor(private userService: UserService, private itemService: ItemService) { }
 
   ngOnInit() {
+    this.rbStack = "normal";
+    this.rbTemporary = "perma";
+
+    this.itemFromPanel = { levelStrength: -1 } as ItemPanel;
+    this.item = { gradeID: -1 } as Item;
+
+    this.optionsRarity = [
+      { name: "Raridade", value: -1 },
+      { name: "Normal", value: 0 },
+      { name: "Raro", value: 1 },
+      { name: "Épico", value: 2 },
+      { name: "Lendário", value: 3 }
+    ]
+
+    this.optionsStrength = [
+      { name: "Fortificação", value: -1 },
+      { name: "Nível 1", value: 1 },
+      { name: "Nível 2", value: 2 },
+      { name: "Nível 3", value: 3 },
+      { name: "Nível 4", value: 4 },
+      { name: "Nível 5", value: 5 },
+      { name: "Nível 6", value: 6 },
+      { name: "Nível 7", value: 7 },
+      { name: "Nível 8", value: 8 },
+      { name: "Nível 9", value: 9 },
+      { name: "Nível 10", value: 10 },
+      { name: "Nível 11", value: 11 },
+      { name: "Nível 12", value: 12 },
+      { name: "Nível 13", value: 13 },
+      { name: "Nível 14", value: 14 },
+      { name: "Nível 15", value: 15 },
+      { name: "Nível 16", value: 16 },
+      { name: "Nível 17", value: 17 }
+    ]
+
   }
 
   findUserByLogin(form: NgForm) {
@@ -77,52 +121,42 @@ export class ItensComponent implements OnInit {
 
   parseTextArea() {
     this.arrayItems = this.textAreaItems.split("\n");
- }
-
- newItemFromPanel(form: NgForm){
-   console.log(this.arrayItems)
-   this.arrayItems.forEach(i => {
-    console.log("item do array:" + i);
-      this.saveItem(form, i);
-   })
- }
-
- saveItem(form, i){
-  this.item.loginUID = this.user.loginUID;
-  this.item.itemID = i;
-  this.itemFromPanel.item = this.item;
-
-  if(!this.validateNewItem()){
-    return;
   }
-  console.log("Objeto a ser inserido:", this.itemFromPanel)
 
-  this.itemService.newItemFromPanel(this.itemFromPanel).subscribe(
-    resultado => {
-      console.log(resultado)
-      this.itemFromPanel = resultado;
-      alert('Item ' + this.itemFromPanel.item.itemID + ' inserido ao jogador ' + this.user.login + ' com sucesso!')
-      this.cleanFormSucess(form);
-    },
-    erro => {
-      if (erro.status != 200) {
-        alert('Erro ao inserir item!');
-        this.cleanForm(form);
-      }
+  newItemFromPanel(form: NgForm) {
+    console.log(this.arrayItems)
+
+    if (this.arrayItems != undefined && this.arrayItems.length > 0) {
+      this.arrayItems.forEach(i => {
+        console.log("item do array:" + i);
+        this.saveItem(form, i);
+      })
+    } else {
+      alert("Erro ao inserir item: Id do item inválido.")
+      return;
     }
-  );
+  }
 
-}
-
-  oldsendDateFromInsertItem(form: NgForm) {
+  saveItem(form, i) {
     this.item.loginUID = this.user.loginUID;
+    this.item.itemID = i;
     this.itemFromPanel.item = this.item;
 
-    console.log("Qtd itens na lista:" + this.arrayItems.length + "  Lista de itens a ser inserida:", this.arrayItems)
+    if (this.rbTemporary == "temporary") {
+      this.itemFromPanel.timeItem = this.timeItemInput;
+    } else {
+      this.itemFromPanel.timeItem = undefined;
+    }
 
+    if (this.rbStack == "carga") {
+      this.itemFromPanel.amountStack = this.amountStackInput;
+    } else {
+      this.itemFromPanel.levelStrength = -1; //reset value fortify
+      this.itemFromPanel.amountStack = undefined; //reset value stack
+    }
 
-
-    if(!this.validateNewItem()){
+    if (!this.validateNewItem()) {
+      this.ngOnInit();
       return;
     }
     console.log("Objeto a ser inserido:", this.itemFromPanel)
@@ -142,9 +176,39 @@ export class ItensComponent implements OnInit {
       }
     );
 
-
-    // this.itemFromPanel.levelStrength =
   }
+
+  // oldsendDateFromInsertItem(form: NgForm) {
+  //   this.item.loginUID = this.user.loginUID;
+  //   this.itemFromPanel.item = this.item;
+
+  //   console.log("Qtd itens na lista:" + this.arrayItems.length + "  Lista de itens a ser inserida:", this.arrayItems)
+
+
+
+  //   if (!this.validateNewItem()) {
+  //     return;
+  //   }
+  //   console.log("Objeto a ser inserido:", this.itemFromPanel)
+
+  //   this.itemService.newItemFromPanel(this.itemFromPanel).subscribe(
+  //     resultado => {
+  //       console.log(resultado)
+  //       this.itemFromPanel = resultado;
+  //       alert('Item ' + this.itemFromPanel.item.itemID + ' inserido ao jogador ' + this.user.login + ' com sucesso!')
+  //       this.cleanFormSucess(form);
+  //     },
+  //     erro => {
+  //       if (erro.status != 200) {
+  //         alert('Erro ao inserir item!');
+  //         this.cleanForm(form);
+  //       }
+  //     }
+  //   );
+
+
+  //   // this.itemFromPanel.levelStrength =
+  // }
 
   //   if (this.user.loginUID !== undefined) {
   //     this.itemService.newItemFromPanel(this.user).subscribe(() => {
@@ -187,31 +251,33 @@ export class ItensComponent implements OnInit {
     if (this.itemFromPanel.item.itemID == undefined
       || this.itemFromPanel.item.itemID.toString() == ""
       || this.itemFromPanel.item.itemID.toString().length <= 3) {
-      alert("Erro ao inserir item: Id do item inválido -> "+ this.itemFromPanel.item.itemID)
+      alert("Erro ao inserir item: Id do item inválido -> " + this.itemFromPanel.item.itemID)
       return false;
     }
 
     if (this.itemFromPanel.item.gradeID == undefined
       || (
         this.itemFromPanel.item.gradeID !== undefined
-        && this.itemFromPanel.item.gradeID < 0
-        && this.itemFromPanel.item.gradeID > 3
+        && (this.itemFromPanel.item.gradeID < 0
+          || this.itemFromPanel.item.gradeID > 3)
       )
       ||
       this.itemFromPanel.item.gradeID.toString() == ""
     ) {
-      alert("Erro ao inserir item: Raridade inválida")
+      alert("Erro ao inserir item: Raridade inválida para item -> " + this.itemFromPanel.item.itemID);
       return false;
     }
 
     if (
       (this.itemFromPanel.levelStrength !== undefined
-        && this.itemFromPanel.levelStrength < 0
-        && this.itemFromPanel.levelStrength > 17)
+        && this.itemFromPanel.levelStrength != -1
+        && (this.itemFromPanel.levelStrength < 0
+          || this.itemFromPanel.levelStrength > 17))
     ) {
-      alert("Erro ao inserir item: Fortificação inválida")
+      alert("Erro ao inserir item: Fortificação inválida para item -> " + this.itemFromPanel.item.itemID);
       return false;
     }
+
     return true;
 
   }
